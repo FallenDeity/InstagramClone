d7e55460fad3e7222bf1b50ae0875ab85b452d5b
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-06-28 18:19:40 +0530

    feat: replace image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -9,11 +9,10 @@ export default function CustomHead({ title }: { title: string }): React.JSX.Elem
 			<meta name="theme-color" content="#ed36ff" />
 			<meta name="keywords" content="instagram, clone, next.js, tailwind, css, react, recoil, next-auth" />
 			<meta name="author" content="Triyan Mukherjee" />
-			<meta property="og:title" content="Triyan's Portfolio" />
+			<meta property="og:title" content="Instagram clone" />
+			<meta property="og:description" content="A basic Instagram clone built with Next.js and Tailwind CSS" />
 			<meta property="og:type" content="website" />
 			<meta property="og:image" content="/logo.png" />
-			<meta property="og:image:width" content="1400" />
-			<meta property="og:image:height" content="900" />
 			<link rel="icon" href="/favicon.ico" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 		</Head>
